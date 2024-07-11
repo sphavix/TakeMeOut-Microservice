a37def059ac7bcb1f6f0e8b857c1da66ceafeabb
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-11 18:12:39 +0200

    Added Authorization with JWT and Identity

--- Dinner.Api/Program.cs
+++ Dinner.Api/Program.cs
@@ -1,9 +1,78 @@
+using Dinner.Infrastructure.DataStore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new() { Title = "Take To Dinner", Version = "v1" });
+
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            }, Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("AdminManagerUserPolicy", options => {
+        options.RequireAuthenticatedUser();
+        options.RequireRole("admin", "manager", "user");
+    })
+    .AddPolicy("AdminManagerPolicy", options =>
+    {
+        options.RequireAuthenticatedUser();
+        options.RequireRole("admin", "manager");
+    })
+    .AddPolicy("AdminUserPolicy", options => {
+        options.RequireAuthenticatedUser();
+        options.RequireRole("admin", "user");
+    });
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager().AddRoles<IdentityRole>();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
 
 var app = builder.Build();
 
@@ -15,5 +84,25 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapControllers();
+
+// create and destroy scopped service when done using them.
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+// Migrate and seed database with data.
+try
+{
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
+    //await SeedData.Seed(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while applying migrations");
+}
 app.Run();
