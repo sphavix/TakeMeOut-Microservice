4d309e1a62c227b84d1eb7e933e1e96da814179d
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-11 20:55:27 +0200

    Seeding Data

---
+++ Dinner.Infrastructure/DataStore/SeedData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinner.Infrastructure.DataStore
+{
+    public class SeedData
+    {
+        public static async Task InitializeLunch(IServiceProvider provider)
+        {
+            ApplicationDbContext context = provider.GetService<ApplicationDbContext>();
+        }
+    }
+}
